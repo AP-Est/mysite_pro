313a516d3a64d7abe2c93f8fa148ffe06958b2f3
Est <estskif@gmail.com>
2022-08-04 21:56:20 +0400

    Optimization Blog block

--- blog/views.py
+++ blog/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from .models import Blog
 
 def all_blogs(request):
-    blogs = Blog.objects.all()
+    blogs = Blog.objects.order_by('-pub_date')[:5]
     return render(request, 'blog/all_blogs.html', {'blogs': blogs})
 
