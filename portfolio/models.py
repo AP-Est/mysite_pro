4e5690a0ec22f2d8d2f73ebfdc92e16dc3089962
Est <estskif@gmail.com>
2022-08-04 21:38:14 +0400

    Add Blog structure

--- portfolio/models.py
+++ portfolio/models.py
@@ -6,3 +6,10 @@ class Project(models.Model):
     image = models.ImageField(upload_to='portfolio/images/')
     url = models.URLField(blank=True)
 
+# class Blog(models.Model):
+#     title = models.CharField(max_length=100)
+#     pub_date = models.DateField
+#     description = models.CharField(max_length=250)
+#     # image = models.ImageField(upload_to='blog/images/')
+#     url = models.URLField(blank=True)
+
